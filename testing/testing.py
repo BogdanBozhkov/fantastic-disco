2354fc2e41d3e6f01fb20aedfe6fe8940324fcfb
BogdanBozhkov <110074564+BogdanBozhkov@users.noreply.github.com>
2023-01-11 10:54:32 -0500

    update.py

--- testing/testing.py
+++ testing/testing.py
@@ -1,13 +1,22 @@
 import os
 import openai
 import base64
+import time
 
-openai.api_key = "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"
+openai.api_key = "sk-NYRrDSU5U64jLzFVVlbbTsT3BlbkFJBFYCf5z7vGXA4tjYD3rN"
 
 response = openai.Image.create(
     prompt = "red apple",
-    n=2,
+    n=1,
     size="1024x1024",
+    response_format = "b64_json"
 )
 
 print(response)
+
+for i in range(0, len(response['data'])):
+    b64 = response['data'][i]['b64_json']
+    filename = f'image_{int(time.time())}_{i}.png'
+    print("Saving file" + filename)
+    with open(f'image_{i}.png', 'wb') as f:
+        f.write(base64.urlsafe_b64decode(b64))
